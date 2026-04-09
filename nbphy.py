23ef34347bcfa872ebfcffe619d80055b52c0411
tc0512 <tancheng_0812@qq.com>
2026-04-09 14:44:53 +0000

    添加时间类

--- nbphy.py
+++ nbphy.py
@@ -13,3 +13,22 @@ class Displacement: #距离s，传入m
         return self.val*1000000
     def nanometre(self):
         return self.val*1000000000
+class Time: #时间t，传入s
+    def __init__(self, val):
+        self.val = val
+    def century(self):
+        return self.val/3153600000
+    def year(self):
+        return self.val/31536000
+    def day(self):
+        return self.val/86400
+    def hour(self):
+        return self.val/3600
+    def minute(self):
+        return self.val/60
+    def millisecond(self):
+        return self.val*1000
+    def microsecond(self):
+        return self.val*1000000
+    def nanosecond(self):
+        return self.val*1000000000
