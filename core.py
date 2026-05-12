2c40b33b2f08f43a7ffd68a40719526bcfbe1129
tc0512 <tancheng_0812@qq.com>
2026-05-12 14:10:42 +0000

    修复core.py的单位换算bug

--- core.py
+++ core.py
@@ -1,36 +1,36 @@
 def displacement(val, unit: str):
     if unit=="km":
-        return val/1000
+        return val*1000
     elif unit=="m":
         return val
     elif unit=="dm":
-        return val*10
+        return val/10
     elif unit=="cm":
-        return val*100
+        return val/100
     elif unit=="mm":
-        return val*1000
+        return val/1000
     elif unit=="μm":
-        return val*1000000
+        return val/1000000
     elif unit=="nm":
-        return val*1000000000
+        return val/1000000000
     raise NotImplementedError("unknown unit")
 def time(val, unit: str):
     if unit=="h":
-        return val/3600
+        return val*3600
     elif unit=="min":
-        return val/60
+        return val*60
     elif unit=="s":
         return val
     elif unit=="ms":
-        return val*1000
+        return val/1000
     elif unit=="μs":
-        return val*1000000
+        return val/1000000
     raise NotImplementedError("unknown unit")
 def velocity(val, unit: str):
     if unit=="m/s":
-        return unit
+        return val
     elif unit=="km/h":
-        return unit/3.6
+        return val*3.6
     raise NotImplementedError("unknown unit")
 def density(val, unit: str):
     if unit=="g/cm^3":
@@ -53,4 +53,4 @@ def power(val, unit: str):
     if unit=="W":
         return val
     elif unit=="kW":
-        return val/1000
+        return val*1000
